6927f602988274e755752359276b5645d727e9d3
Tatyana Amosova <atgatg@bk.ru>
2023-09-15 15:59:24 +0300

    FileReader

--- src/components/PhotoManager.tsx
+++ src/components/PhotoManager.tsx
@@ -4,16 +4,22 @@ import { Photo } from "./Photo"
 export const PhotoManager = () => {
 	const [dataUrl, setDataUrl] = useState<string[]>([]);
 
-	const fileToDataUrl = (file: Blob) => {
+	const fileToDataUrl = (file: Blob): Promise<string> => {
 		return new Promise((resolve, reject) => {
 			const fileReader = new FileReader();
 		
-			fileReader.addEventListener('load', evt => {
-				resolve(evt.currentTarget?.result);
+			fileReader.addEventListener('load', (evt: ProgressEvent<FileReader>) => {
+				if (evt.currentTarget && "result" in evt.currentTarget) {
+					const result = evt.currentTarget.result as string;
+					resolve(result);
+				}
 			});
 			
-			fileReader.addEventListener('error', evt => {
-				reject(new Error(evt.currentTarget?.error));
+			fileReader.addEventListener('error', (evt: ProgressEvent<FileReader>) => {
+				if (evt.currentTarget && "error" in evt.currentTarget) {
+					const error = evt.currentTarget.error as string;
+					reject(new Error(error));
+				}
 			});
 			
 			fileReader.readAsDataURL(file);
@@ -23,7 +29,7 @@ export const PhotoManager = () => {
 	const handleSelect = async (evt: React.ChangeEvent<HTMLInputElement>) => {
 		if (evt.target.files?.length) {
 			const files = [...evt.target.files];
-			const urls: 'string[]' = await Promise.all(files.map(o => fileToDataUrl(o)));
+			const urls: string[] = await Promise.all(files.map(o => fileToDataUrl(o)));
 			// У вас в массиве - dataUrl, можете использовать в качестве значения атрибута src тега img
 			setDataUrl([...dataUrl, ...urls]);
 		}
